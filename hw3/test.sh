f026efe1503a36077fda3ebf897a01510fe6150d
Herman <Herman>
2016-12-18 00:02:51 +0800

    Bug fix: change script format into UNIX

--- hw3/test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-KERAS_BACKEND=theano THEANO_FLAGS='floatX=float32,device=gpu0,lib.cnmem=1' python2 mytest-semi2.py $1 $2 $3
--- hw3/train.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-KERAS_BACKEND=theano THEANO_FLAGS='floatX=float32,device=gpu0,lib.cnmem=1' python2 mytrain-semi2.py $1 $2
